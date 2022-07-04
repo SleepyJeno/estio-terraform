38eceda3540156957fd61a61457ed02129eeb9a1
Jeno Kalinszki <JenoKalinszki@tengroup.com>
2022-07-04 16:07:37 +0100

    support for ansible

--- main.tf
+++ main.tf
@@ -34,6 +34,27 @@ resource "aws_security_group" "estio-ec2-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "db_sg_from_vpc" {
+  name        = "estio-db-sg"
+  description = "Allow 3306"
+  vpc_id      = "vpc-3da39155"
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.estio-ec2-sg]
+  }
+
+  egress {
+    to_port     = 0
+    from_port   = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "ansible_controller" {
   ami                         = "ami-0015a39e4b7c0966f"
   instance_type               = "t2.micro"
@@ -41,8 +62,8 @@ resource "aws_instance" "ansible_controller" {
   subnet_id                   = "subnet-b0a335fc"
   vpc_security_group_ids      = [aws_security_group.estio-ec2-sg.id]
   associate_public_ip_address = true
-  user_data                   = file("scripts/ansible_install.sh")
-  #user_data                   = file("./apache.sh")
+  # user_data                   = file("scripts/ansible_install.sh") #uncomment for ansible setup
+  #user_data                   = file("./apache.sh") #uncomment for apache server 
 
   tags = {
     Name = "ansible_controller"
@@ -56,8 +77,9 @@ resource "aws_instance" "ansible_host" {
   subnet_id                   = "subnet-b0a335fc"
   vpc_security_group_ids      = [aws_security_group.estio-ec2-sg.id]
   associate_public_ip_address = true
-  user_data                   = file("scripts/host_setup.sh")
-  #user_data                   = file("./flask.sh")
+  # user_data                   = file("./flask.sh") #flask app setup
+  # user_data                   = file("scripts/host_setup.sh") #ansible setup
+
 
   tags = {
     Name = "ansible_host"
